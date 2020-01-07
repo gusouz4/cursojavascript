9ada6b450a9a8ce668b3dc5fdf728bc998f8e5dc
gusouz4 <gusouza.1@hotmail.com>
2020-01-07 13:42:18 -0300

    loading before request usuarioGithub

--- Desafios Modulo4/usuarioGithub.js
+++ Desafios Modulo4/usuarioGithub.js
@@ -4,10 +4,11 @@ var listElement = document.querySelector('#app ul');
 
 var buscaGit = function () {
     return new Promise(function (resolve, reject) {
+        renderLoading();
         axios.get('https://api.github.com/users/'+ inputElement.value +'/repos')
             .then(function (response) {
                 listElement.innerHTML = '';
-                console.log(response);
+        
                 for(repo of response.data){
                     var repoElement = document.createElement('li');
                     var repoText = document.createTextNode(repo.name);
@@ -15,11 +16,23 @@ var buscaGit = function () {
                     repoElement.appendChild(repoText);
 
                     listElement.appendChild(repoElement);
+
                 }
             })
             .catch(function (error) {
+                if(axios.get('https://api.github.com/users'+ inputElement.value + '/repos' == false)){
+                    alert('Erro 404: Usuário do GitHub não existente');
+                }
                 console.warn(error);
             });
 
     });
 }
+
+function renderLoading(loading) {
+    listElement.innerHTML = "";
+    var textElement = document.createTextNode("Carregando...");
+    var loadingElement = document.createElement("li");
+    loadingElement.appendChild(textElement);
+    listElement.appendChild(loadingElement);
+  }
